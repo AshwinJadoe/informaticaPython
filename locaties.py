051636ab7a2e3f8e7e291e234bf04c3d18e817da
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-06-14 10:05:52 +0200

    begin aan lijst met locaties

---
+++ locaties.py
@@ -0,0 +1,108 @@
+location = {
+  "congiërge" : {
+    "tekst": "Hallo allemaal.",
+    "keuzes": {
+      "n" : "trappenhuis", 
+      "o" : "kluisjes",
+      "w" : "aula"
+      
+  }
+  },
+  "aula" : { 
+    "tekst": "Je bent nu in de aula, helaas zijn alle tafels bezet.", 
+    "keuzes": { 
+      "s" : "scheikunde", 
+      "w" : "kantine", 
+      "o" : "congiërge",
+      "n" : "gymzaal"
+    } 
+  },
+  "trappenhuis1a" : { 
+    "tekst": "Je bent nu in het eerste trappenhuis op de begane grond, wil je naar boven?", 
+    "keuzes": { 
+      "b" : "trappenhuis1b", 
+      "z" : "congiërge"
+    }
+  }, 
+  "kluisjes" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "w" : "congiërge", 
+      "z" : "kunst", 
+      "o" : "trappenhuis2a"
+    }
+  }, 
+  "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : ""
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "trappenhuis1b" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+   "scheikunde" : { 
+    "tekst": "tekst", 
+    "keuzes": { 
+      "n" : 
+      "w" : 
+      "z" : 
+      "o" : 
+    }
+  },
+}
+
