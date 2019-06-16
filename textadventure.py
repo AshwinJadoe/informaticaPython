ff35bd5dfa69527e65a1e4f01ea558e0b96f7c2d
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-06-16 20:45:01 +0200

    location naar main.py geimport

--- textadventure.py
+++ textadventure.py
@@ -1,54 +1,22 @@
-import time 
-import random 
+import time
+import random
+from locaties
+import location
 
-""""print("Welkom bij 'The Road To The Lerarenkamer'! Het is de bedoeling dat je via verschillende locaties in de school de lerarenkamer bereikt. Je begint bij de congiërge, want je bent eens weer te laat gekomen voor het eerste uurtje Assie. Bij elke locatie kan je kiezen tussen 'n', 's', 'w' en 'o' en dan zal je weer bij een nieuwe locatie komen. Ook kan je naar de volgende verdieping via 1 van de trappenhuizen.")
-
-
-location = {
-  "congiërge" : {
-    "tekst": "Hallo allemaal.",
-    "keuzes": {
-      "n" : "trappenhuis1a", 
-      "o" : "kluisjes",
-      "w" : "aula"
-  }
-  },
-  "aula" : { 
-    "tekst": "Je bent nu in de aula, helaas zijn alle tafels bezet.", 
-    "keuzes": { 
-      "s" : "scheikunde", 
-      "w" : "kantine", 
-      "o" : "congiërge",
-      "n" : "gymzaal"
-    } 
-  },
-  "trappenhuis1a" : { 
-    "tekst": "Je bent nu in het eerste trappenhuis op de begane grond, wil je naar boven?", 
-    "keuzes": { 
-      "b" : "trappenhuis1b", 
-      "s" : "congiërge"
-    }
-  }, 
-}"""
-
-
+  ""
+""
+print("Welkom bij 'The Road To The Lerarenkamer'! Het is de bedoeling dat je via verschillende locaties in de school de lerarenkamer bereikt. Je begint bij de congiërge, want je bent eens weer te laat gekomen voor het eerste uurtje Assie. Bij elke locatie kan je kiezen tussen 'n', 's', 'w' en 'o' en dan zal je weer bij een nieuwe locatie komen. Ook kan je naar de volgende verdieping via 1 van de trappenhuizen.")
+""
+"
 
 current_room = "congiërge"
 
-
-def engine (room):
-  print (location[room]["tekst"])
-  actie = input("en nu? ").lower()
-  for x in (location[room]["keuzes"]):
-    if actie == x:
-      current_room = location[room]["keuzes"][actie]
-      engine(current_room)   
-
+def engine(room):
+  print(location[room]["tekst"])
+actie = input("en nu? ").lower()
+for x in (location[room]["keuzes"]):
+  if actie == x:
+  current_room = location[room]["keuzes"][actie]
 engine(current_room)
 
-
-
-
-
-
-
+engine(current_room)
