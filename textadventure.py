1d9cfbd9b8f56b31747a0a5ec918d66144197a60
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-06-24 21:25:57 +0200

    update roomcheck functie

--- textadventure.py
+++ textadventure.py
@@ -6,11 +6,13 @@ from locaties import location
 
 #print("Welkom bij 'The Road To The Lerarenkamer'! Het is de bedoeling dat je via verschillende locaties in de school de lerarenkamer bereikt. Je begint bij de congiërge, want je bent eens weer te laat gekomen voor het eerste uurtje Assie. Bij elke locatie kan je kiezen tussen 'n', 's', 'w' en 'o' en dan zal je weer bij een nieuwe locatie komen. Ook kan je naar de volgende verdieping via 1 van de trappenhuizen.")
 
-inventory = [" "]
+inventory = ["zaklamp"]
 commands_list = ["n", "z", "w", "o", "b", "d"]
 
 current_room = "congiërge"
-oude_kamer = "congiërge"
+begin_kamer = "congiërge"
+levens = 1
+
 
 def roomcheck(room):
    for x in location[room]:
@@ -20,7 +22,11 @@ def roomcheck(room):
         if i == location[room]["obstakel"]["obstakel_object"]:
           return False
         else:
+          print (location[room]["obstakel"]["obstakel_niet"])
           return True
+    else:
+      return False
+
 
 def muur(x,room):
   return (location[room]["keuzes"][x]) == "nee"
@@ -38,15 +44,10 @@ def commands(input):
 def engine (room):
   global current_room
   if roomcheck(room): 
-    global oude_kamer
-    print(location[room]["obstakel"]["obstakel_tekst"])
-    engine(oude_kamer)
+      engine (begin_kamer)
   else:  
-    oude_kamer = room
-    print (oude_kamer)
     print (location[room]["tekst"])
     actie = input("en nu? ").lower()
-    print (commands(actie))
     if commands(actie) == True:
       for x in (location[room]["keuzes"]):
         if actie == x:  
