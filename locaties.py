3ccc456a6ee708d86f7a507e8b4fe1ab41a9b00c
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-06-27 20:32:40 +0200

    update locaties

--- locaties.py
+++ locaties.py
@@ -2,82 +2,124 @@
 location = {
   "congiërge": {
     "tekst": "Je bent nu bij de conciërge. Ben je nou alweer te laat gekomen?",
-     "obstakel" : "nee",
     "keuzes": {
       "n": "trappenhuis1a",
       "o": "kluisjes",
       "w": "aula",
-      "z": "nee"
-
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
 
  "aula": { 
     "tekst": "Je bent nu in de aula, helaas zijn alle tafels bezet.",
-    "obstakel" : "nee",
+    "obstakel" : {
+      "obstakel_tekst" : "Je moet zaklamp hebbe",
+      "obstakel_object" : "zaklamp",
+      "obstakel_bezit" : "aight je hebt dees gewoon",
+      "obstakel_niet" : "nope je hebt m niet je moet terug"
+    },
     "keuzes": {
       "z": "scheikunde",
       "w": "kantine",
       "o": "congiërge",
-      "n": "gymzaal"
+      "n": "gymzaal",
+      "b": "nee",
+      "d": "nee",
+      
     }
   },
   
   "trappenhuis1a": {
     "tekst": "Je bent nu in het eerste trappenhuis op de begane grond, wil je naar boven?",
-    "obstakel" : "nee",
     "keuzes": {
       "b": "trappenhuis1b",
-      "z": "congiërge"
+      "z": "congiërge",
+      "n": "nee",
+      "o": "nee",
+      "z": "nee",
+      "w": "nee",
     }
   },
   "kluisjes": {
     "tekst": "Moet je boeken of je jas in je kluisje doen? Nou dit is je kans, maar welk kluisje is van jou en wat is de code van je kluisje?",
-    "obstakel" : "nee",
+
     "keuzes": {
       "w": "congiërge",
       "z": "kunst",
-      "o": "trappenhuis2a"
+      "o": "trappenhuis2a",
+      "n": "nee",
+      "b": "nee",
+      "d": "nee"
+      
     }
   },
   "scheikunde": {
     "tekst": "Dit is nou het scheikundelokaal! PAS OP ONTPLOFFINGSGEVAAR! Ooh daar is Stapel. Stapel: 'Hallo, los deze reactievergelijking eens op'",
     "keuzes": {
       "n": "aula",
-      "z": "trappenhuis3a"
+      "z": "trappenhuis3a",
+      "o": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
+      
     }
   },
   "trappenhuis3a": {
     "tekst": "Wat handig dat hier ook een trappenhuis zit. Wil je naar boven?",
     "keuzes": {
       "n": "scheikunde",
-      "b": "trappenhuis3b"
+      "b": "trappenhuis3b",
+      "o": "nee",
+      "z": "nee",
+      "w": "nee",
+      "d": "nee"
     }
   },
   "kantine": {
     "tekst": "Hier is de kantine, waar je bij de 'gezonde cafetaria' hele lekkere panini's, wafels en kroketten kan halen.",
-    "obstakel" : "ja",
     "keuzes": {
-      "o": "aula"
+      "o": "aula",
+      "n": "nee",
+      "w": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "gymzaal": {
     "tekst": "Lekker even sporten, want je bent nu in de gymzaal",
     "keuzes": {
-      "z": "aula"
+      "z": "aula",
+      "o": "nee",
+      "n": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "handvaardigheid": {
     "tekst": "Ben je eigenlijk een beetje creatief. Dan heeft Nobrega wel een opdracht voor jou!",
     "keuzes": {
-      "n": "kluisjes"
+      "n": "kluisjes",
+      "o": "nee",
+      "z": "nee",
+      "w": "nee",
+      "d": "nee",
+      "b": "nee"
     }
   },
   "trappenhuis2a": {
     "tekst": "Nog een trappenhuis, wil je naar boven?",
     "keuzes": {
       "b": "trappenhuis2b",
-      "w": "kluisjes"
+      "w": "kluisjes",
+      "d": "nee",
+      "n": "nee",
+      "o": "nee",
+      "z": "nee"
     }
   },
   "trappenhuis1b": {
@@ -87,7 +129,8 @@ location = {
       "w": "aula balustrade",
       "z": "informatica",
       "o": "Wal",
-      "d": "trappenhuis1a"
+      "d": "trappenhuis1a",
+      "n": "nee"
     }
   },
   "aula balustrade": {
@@ -95,68 +138,110 @@ location = {
     "keuzes": {
       "n": "Vlaam",
       "w": "biologie",
-      "o": "trappenhuis1b"
+      "o": "trappenhuis1b",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "Vlaam": {
-    "tekst": "Oh nee, Oh nee, daar is Vlaam snel ren weg nu het nog kan. Anders moet je misschien nablijven.",
+    "tekst": "Oh nee, Oh nee, daar is mevrouw Vlaam snel ren weg nu het nog kan. Anders moet je misschien nablijven.",
     "keuzes": {
       "n": "lerarenkamer",
-      "z": "aula balustrade"
+      "z": "aula balustrade",
+      "o": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "lerarenkamer": {
     "tekst": "Ja, ja. Hier is het dan, de lerarenkamer!!!!",
     "keuzes": {
-      "z": "Vlaam"
+      "z": "Vlaam",
+      "o": "nee",
+      "n": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "biologie": {
     "tekst": "Cellen, koolstofcirculatie en ecosystemen. Dit zijn allemaal hele interessante onderwerpen. Als je biologie kiest mag je over al deze dingen leren, geweldig toch?",
     "keuzes": {
       "z": "natuurkunde",
-      "o": "aula balustrade"
+      "o": "aula balustrade",
+      "n": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "natuurkunde": {
     "tekst": "Skip die Kechs ik wil Meijer!! PAS OP MISSCHIEN WORD JE GEËLEKTROCUTEERD, DOOR DEZE NATUURKUNDELERAAR!",
     "keuzes": {
       "n": "biologie",
-      "z": "trappenhuis3b"
+      "z": "trappenhuis3b",
+      "o": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "trappenhuis3b": {
     "tekst": "Je bent op de eerste verdieping in het trappenhuis, waar wil je naar toe?",
     "keuzes": {
       "n": "natuurkunde",
-      "d": "trappenhuis3a"
+      "d": "trappenhuis3a",
+      "o": "nee",
+      "z": "nee",
+      "w": "nee",
+      "b": "nee"
     }
   },
   "informatica": {
     "tekst": "Het beste vak ooit is hier, namelijk Informatica",
     "keuzes": {
       "n": "trappenhuis1b",
-      "z": "mediatheek"
+      "z": "mediatheek",
+      "o": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "mediatheek": {
     "tekst": "SSSSSSSSSSSSSTTTTT, stilte in de Mediatheek",
     "keuzes": {
       "n": "informatica",
+      "o": "nee",
+      "w": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
+
     }
   },
   "Wal": {
     "tekst": "Wallieeee, hoe gaat het?",
     "keuzes": {
       "o": "aardrijkskunde",
-      "w": "trappenhuis1b"
+      "w": "trappenhuis1b",
+      "n": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "aardrijkskunde": {
     "tekst": "Dit is AK",
     "keuzes": {
       "o": "trappenhuis2b",
-      "w": "Wal"
+      "w": "Wal",
+      "n": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "trappenhuis2b": {
@@ -165,20 +250,30 @@ location = {
       "z": "geschiedenis",
       "w": "aardrijkskunde",
       "b": "trappenhuis2c",
-      "d": "trappenhuis2a"
+      "d": "trappenhuis2a",
+      "n": "nee",
+      "o": "nee",
     }
   },
   "geschiedenis": {
     "tekst": "Probeer niet in slaap te vallen. Je bent bij geschiedenis.",
     "keuzes": {
-      "n": "trappenhuis2b"
+      "n": "trappenhuis2b",
+      "o": "nee",
+      "z": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "trappenhuis1c": {
     "tekst": "Je bent in een trappenhuis op de tweede verdieping.",
     "keuzes": {
       "b": "trappenhuis1d",
+      "n": "nee",
       "o": "wiskunde",
+      "z": "nee",
+      "w": "nee",
       "d": "trappenhuis1b"
     }
   },
@@ -186,14 +281,22 @@ location = {
     "tekst": "De oppervlakte van een cirkel is πr^2, wiskunde.",
     "keuzes": {
       "w": "trappenhuis1c",
-      "o": "nederlands"
+      "o": "nederlands",
+      "n": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "nederlands": {
     "tekst": "Wollla doet jij goeder Nederaldns sprieken als ik?",
     "keuzes": {
       "w": "wiskunde",
-      "w": "trappenhuis2c"
+      "o": "trappenhuis2c",
+      "n": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "trappenhuis2c": {
@@ -209,41 +312,67 @@ location = {
     "tekst": "You Spiek verie goet Inglish",
     "keuzes": {
       "n": "trappenhuis2c",
-      "w": "collegezaal"
+      "w": "collegezaal",
+      "o": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee",
     }
   },
   "collegezaal": {
     "tekst": "Ooh er zijn examens bezig in de collegezaal, snel weg.",
     "keuzes": {
       "o": "engels",
-      "w": "Assendorp"
+      "w": "Assendorp",
+      "z": "nee",
+      "n": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "Assendorp": {
     "tekst": "AAAAAAAHH, Assendorp, snel rennen!",
     "keuzes": {
-      "o": "collegezaal"
+      "o": "collegezaal",
+      "n": "nee",
+      "z": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "trappenhuis1d": {
     "tekst": "Je bent in een trappenhuis op de derde verdieping",
     "keuzes": {
       "o": "duits",
-      "d": "trappenhuis1c"
+      "d": "trappenhuis1c",
+      "n": "nee",
+      "z": "nee",
+      "w": "nee",
+      "b": "nee"
     }
   },
   "duits": {
     "tekst": "Guten Tag",
     "keuzes": {
       "o": "klassieke talen",
-      "w": "trappenhuis1d"
+      "n": "nee",
+      "z": "nee",
+      "w": "trappenhuis1d",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "klassieke talen": {
     "tekst": "Zit jij op het gymnasium? Spreek jij toevallig Griek en Latijns? HET ANTWOORD IS NEE, JE LEERT NIET OM TE SPREKEN HET IS EEN DODE TAAL!!!!",
     "keuzes": {
       "o": "trappenhuis2d",
-      "w": "duits"
+      "w": "duits",
+      "n": "nee",
+      "z": "nee",
+      "d": "nee",
+      "b": "nee",
+      "n": "nee"
     }
   },
   "trappenhuis2d": {
@@ -258,14 +387,22 @@ location = {
     "tekst": "Bonjour, je bent bij Frans",
     "keuzes": {
       "z": "tekenen",
-      "n": "trappenhuis2d"
+      "n": "trappenhuis2d",
+      "o": "nee",
+      "w": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
   "tekenen": {
     "tekst": "Je kan dit vergeten, tekenen is toch een nutteloos vak",
     "keuzes": {
       "n": "frans",
-      "o": "branduitgang!!!"
+      "o": "nee",
+      "w": "nee",
+      "z": "nee",
+      "b": "nee",
+      "d": "nee"
     }
   },
 }
