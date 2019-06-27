d9ac23c9bb662e9e81235f9901569df29b23d873
vriezo <45482682+vriezo@users.noreply.github.com>
2019-06-27 17:18:56 +0200

    Update locaties.py
    Ik heb de tekst bij de locaties ingevuld. Nu staat er bij alles een stukje tekst

--- locaties.py
+++ locaties.py
@@ -1,7 +1,7 @@
 
 location = {
   "congiërge": {
-    "tekst": "Hallo allemaal.",
+    "tekst": "Je bent nu bij de conciërge. Ben je nou alweer te laat gekomen?",
      "obstakel" : "nee",
     "keuzes": {
       "n": "trappenhuis1a",
@@ -16,7 +16,7 @@ location = {
     "tekst": "Je bent nu in de aula, helaas zijn alle tafels bezet.",
     "obstakel" : "nee",
     "keuzes": {
-      "s": "scheikunde",
+      "z": "scheikunde",
       "w": "kantine",
       "o": "congiërge",
       "n": "gymzaal"
@@ -32,7 +32,7 @@ location = {
     }
   },
   "kluisjes": {
-    "tekst": "tekst",
+    "tekst": "Moet je boeken of je jas in je kluisje doen? Nou dit is je kans, maar welk kluisje is van jou en wat is de code van je kluisje?",
     "obstakel" : "nee",
     "keuzes": {
       "w": "congiërge",
@@ -41,47 +41,47 @@ location = {
     }
   },
   "scheikunde": {
-    "tekst": "tekst",
+    "tekst": "Dit is nou het scheikundelokaal! PAS OP ONTPLOFFINGSGEVAAR! Ooh daar is Stapel. Stapel: 'Hallo, los deze reactievergelijking eens op'",
     "keuzes": {
       "n": "aula",
       "z": "trappenhuis3a"
     }
   },
   "trappenhuis3a": {
-    "tekst": "tekst",
+    "tekst": "Wat handig dat hier ook een trappenhuis zit. Wil je naar boven?",
     "keuzes": {
       "n": "scheikunde",
       "b": "trappenhuis3b"
     }
   },
   "kantine": {
-    "tekst": "tekst",
+    "tekst": "Hier is de kantine, waar je bij de 'gezonde cafetaria' hele lekkere panini's, wafels en kroketten kan halen.",
     "obstakel" : "ja",
     "keuzes": {
       "o": "aula"
     }
   },
   "gymzaal": {
-    "tekst": "tekst",
+    "tekst": "Lekker even sporten, want je bent nu in de gymzaal",
     "keuzes": {
       "z": "aula"
     }
   },
   "handvaardigheid": {
-    "tekst": "tekst",
+    "tekst": "Ben je eigenlijk een beetje creatief. Dan heeft Nobrega wel een opdracht voor jou!",
     "keuzes": {
       "n": "kluisjes"
     }
   },
   "trappenhuis2a": {
-    "tekst": "tekst",
+    "tekst": "Nog een trappenhuis, wil je naar boven?",
     "keuzes": {
       "b": "trappenhuis2b",
       "w": "kluisjes"
     }
   },
   "trappenhuis1b": {
-    "tekst": "tekst",
+    "tekst": "Je bent nu in het trappenhuis op de eerste verdieping, wil je naar boven of beneden of wil je ergens anders naartoe?",
     "keuzes": {
       "b": "trappenhuis 1c",
       "w": "aula balustrade",
@@ -91,7 +91,7 @@ location = {
     }
   },
   "aula balustrade": {
-    "tekst": "tekst",
+    "tekst": "Zo hé, vanaf hier kan je heel de aula zien. Echt te gek!",
     "keuzes": {
       "n": "Vlaam",
       "w": "biologie",
@@ -99,68 +99,68 @@ location = {
     }
   },
   "Vlaam": {
-    "tekst": "tekst",
+    "tekst": "Oh nee, Oh nee, daar is Vlaam snel ren weg nu het nog kan. Anders moet je misschien nablijven.",
     "keuzes": {
       "n": "lerarenkamer",
       "z": "aula balustrade"
     }
   },
   "lerarenkamer": {
-    "tekst": "tekst",
+    "tekst": "Ja, ja. Hier is het dan, de lerarenkamer!!!!",
     "keuzes": {
       "z": "Vlaam"
     }
   },
   "biologie": {
-    "tekst": "tekst",
+    "tekst": "Cellen, koolstofcirculatie en ecosystemen. Dit zijn allemaal hele interessante onderwerpen. Als je biologie kiest mag je over al deze dingen leren, geweldig toch?",
     "keuzes": {
       "z": "natuurkunde",
       "o": "aula balustrade"
     }
   },
   "natuurkunde": {
-    "tekst": "tekst",
+    "tekst": "Skip die Kechs ik wil Meijer!! PAS OP MISSCHIEN WORD JE GEËLEKTROCUTEERD, DOOR DEZE NATUURKUNDELERAAR!",
     "keuzes": {
       "n": "biologie",
       "z": "trappenhuis3b"
     }
   },
   "trappenhuis3b": {
-    "tekst": "tekst",
+    "tekst": "Je bent op de eerste verdieping in het trappenhuis, waar wil je naar toe?",
     "keuzes": {
       "n": "natuurkunde",
       "d": "trappenhuis3a"
     }
   },
   "informatica": {
-    "tekst": "tekst",
+    "tekst": "Het beste vak ooit is hier, namelijk Informatica",
     "keuzes": {
       "n": "trappenhuis1b",
       "z": "mediatheek"
     }
   },
   "mediatheek": {
-    "tekst": "tekst",
+    "tekst": "SSSSSSSSSSSSSTTTTT, stilte in de Mediatheek",
     "keuzes": {
       "n": "informatica",
     }
   },
   "Wal": {
-    "tekst": "tekst",
+    "tekst": "Wallieeee, hoe gaat het?",
     "keuzes": {
       "o": "aardrijkskunde",
       "w": "trappenhuis1b"
     }
   },
   "aardrijkskunde": {
-    "tekst": "tekst",
+    "tekst": "Dit is AK",
     "keuzes": {
       "o": "trappenhuis2b",
       "w": "Wal"
     }
   },
   "trappenhuis2b": {
-    "tekst": "tekst",
+    "tekst": "Je bent in een trappenhuis op de eerste verdieping.",
     "keuzes": {
       "z": "geschiedenis",
       "w": "aardrijkskunde",
@@ -169,13 +169,13 @@ location = {
     }
   },
   "geschiedenis": {
-    "tekst": "tekst",
+    "tekst": "Probeer niet in slaap te vallen. Je bent bij geschiedenis.",
     "keuzes": {
       "n": "trappenhuis2b"
     }
   },
   "trappenhuis1c": {
-    "tekst": "tekst",
+    "tekst": "Je bent in een trappenhuis op de tweede verdieping.",
     "keuzes": {
       "b": "trappenhuis1d",
       "o": "wiskunde",
@@ -183,21 +183,21 @@ location = {
     }
   },
   "wiskunde": {
-    "tekst": "tekst",
+    "tekst": "De oppervlakte van een cirkel is πr^2, wiskunde.",
     "keuzes": {
       "w": "trappenhuis1c",
       "o": "nederlands"
     }
   },
   "nederlands": {
-    "tekst": "tekst",
+    "tekst": "Wollla doet jij goeder Nederaldns sprieken als ik?",
     "keuzes": {
       "w": "wiskunde",
       "w": "trappenhuis2c"
     }
   },
   "trappenhuis2c": {
-    "tekst": "tekst",
+    "tekst": "Je bent in een trappenhuis op de tweede verdieping",
     "keuzes": {
       "z": "engels",
       "w": "nederlands",
@@ -206,48 +206,48 @@ location = {
     }
   },
   "engels": {
-    "tekst": "tekst",
+    "tekst": "You Spiek verie goet Inglish",
     "keuzes": {
       "n": "trappenhuis2c",
       "w": "collegezaal"
     }
   },
   "collegezaal": {
-    "tekst": "tekst",
+    "tekst": "Ooh er zijn examens bezig in de collegezaal, snel weg.",
     "keuzes": {
       "o": "engels",
       "w": "Assendorp"
     }
   },
   "Assendorp": {
-    "tekst": "tekst",
+    "tekst": "AAAAAAAHH, Assendorp, snel rennen!",
     "keuzes": {
       "o": "collegezaal"
     }
   },
   "trappenhuis1d": {
-    "tekst": "tekst",
+    "tekst": "Je bent in een trappenhuis op de derde verdieping",
     "keuzes": {
       "o": "duits",
       "d": "trappenhuis1c"
     }
   },
   "duits": {
-    "tekst": "tekst",
+    "tekst": "Guten Tag",
     "keuzes": {
       "o": "klassieke talen",
       "w": "trappenhuis1d"
     }
   },
   "klassieke talen": {
-    "tekst": "tekst",
+    "tekst": "Zit jij op het gymnasium? Spreek jij toevallig Griek en Latijns? HET ANTWOORD IS NEE, JE LEERT NIET OM TE SPREKEN HET IS EEN DODE TAAL!!!!",
     "keuzes": {
       "o": "trappenhuis2d",
       "w": "duits"
     }
   },
   "trappenhuis2d": {
-    "tekst": "tekst",
+    "tekst": "Je bent in een trappenhuis op de derde verdieping",
     "keuzes": {
       "z": "frans",
       "w": "klassieke talen",
@@ -255,14 +255,14 @@ location = {
     }
   },
   "frans": {
-    "tekst": "tekst",
+    "tekst": "Bonjour, je bent bij Frans",
     "keuzes": {
       "z": "tekenen",
       "n": "trappenhuis2d"
     }
   },
   "tekenen": {
-    "tekst": "tekst",
+    "tekst": "Je kan dit vergeten, tekenen is toch een nutteloos vak",
     "keuzes": {
       "n": "frans",
       "o": "branduitgang!!!"
