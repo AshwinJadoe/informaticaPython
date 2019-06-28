5fc30d1700ef3e9238b0b694e5fcfa0b18a812c4
vriezo <45482682+vriezo@users.noreply.github.com>
2019-06-28 08:55:23 +0200

    Update galgje.py
    woorden en een galg toegevoegd

--- galgje.py
+++ galgje.py
@@ -1,1 +1,24 @@
+import time
+import random
+import sys
 
+#Gallow printouts
+gallows = ["_______\n|     |\n|     O\n|    -|-\n|     |\n|    / \\ \n|\n=======",
+       "_______\n|     |\n|     O\n|    -|-\n|     |\n|    /\n|\n=======",
+       "_______\n|     |\n|     O\n|    -|-\n|     |\n|\n|\n=======",
+       "_______\n|     |\n|     O\n|    -|\n|     |\n|\n|\n=======",
+       "_______\n|     |\n|     O\n|     |\n|     |\n|\n|\n=======",
+       "_______\n|     |\n|     O\n|\n|\n|\n|\n=======",
+       "_______\n|     |\n|\n|\n|\n|\n|\n=======",
+      ]
+
+# Lists containing the words for the game
+four_letters = ['boom', 'deel', 'ding', 'ring', 'snel', 'ruim', 'duim', 'geel', 'eend']
+
+ten_letters = ['afbakbrood', 'agorafobie', 'autodeuren', 'barricades', 'paaseieren', 'xylofonist']
+
+twenty_five_letters = ['arbeidsongeschiktheidswet', 'teambuildingsactiviteiten', 'uraniumverrijkingsfabriek', 'vierentwintiguurseconomie', 'aaaaaaaaaaaaaaaaaaaaaaaaa']
+
+thirty_five_letters= [
+ 'hippopotomonstrosesquippedaliofobie', 'landbouwmechanisatietentoonstelling', 'hottentottententententoonstellingen', 'ultravioletstralingsabsorptiefilter'
+]
