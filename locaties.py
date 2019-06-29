6a91c6e15b778c77ef89a127931406b151441ddb
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-06-29 13:58:32 +0200

    final touches locaties + easter egg

--- locaties.py
+++ locaties.py
@@ -1,6 +1,6 @@
 location = {
   "congiërge": {
-    "tekst": "",
+    "tekst": "We zijn nu bij de congierge",
     "keuzes": {
       "n": "trappenhuis1a",
       "o": "kluisjes",
@@ -13,16 +13,6 @@ location = {
 
  "aula": { 
     "tekst": "Je bent nu in de aula, helaas zijn alle tafels bezet.",
-    "obstakel" : {
-      "obstakel_tekst" : "wollah je hebt zaklamp nodig",
-      "obstakel_object" : "zaklamp",
-      "obstakel_bezit" : "aight je hebt dees gewoon",
-      "obstakel_niet" : "nope je hebt m niet je moet terug"
-    },
-    "object" : {
-      "object_tekst" : "er ligt een banaan",
-      "object_soort" : "banaan"
-    },
     "keuzes": {
       "z": "scheikunde",
       "w": "kantine",
@@ -41,12 +31,16 @@ location = {
       "z": "congiërge",
       "n": "nee",
       "o": "nee",
-      "z": "nee",
       "w": "nee",
     }
   },
   "kluisjes": {
-    "tekst": "Moet je boeken of je jas in je kluisje doen? Nou dit is je kans, maar welk kluisje is van jou en wat is de code van je kluisje?",
+    "tekst": "Oh hier zijn de kluisjes, goed om te weten",
+    "object" : {
+      "object_tekst" : "Hey hier dit kluisje staat open, er ligt een rekenmachine in. Die kan vast later van pas komen ;)",
+      "object_soort" : "rekenmachine"
+
+    },
 
     "keuzes": {
       "w": "congiërge",
@@ -59,12 +53,12 @@ location = {
     }
   },
   "scheikunde": {
-    "tekst": "Dit is nou het scheikundelokaal! PAS OP ONTPLOFFINGSGEVAAR! Ooh daar is Stapel. Stapel: 'Hallo, los deze reactievergelijking eens op'",
+    "tekst": "Welkom bij Scheikunde, het beste vak op aarde",
     "obstakel" : {
-      "obstakel_tekst" : "Hé, jij zou verf voor mij meenemen voor het practicum. Je komt dit lokaal niet binnen totdat je verf hebt voor mij!",
+      "obstakel_tekst" : "Stapel: Hé, jij zou verf voor mij meenemen voor het practicum. Je komt dit lokaal niet binnen totdat je verf hebt voor mij!",
       "obstakel_object" : "verf",
-      "obstakel_bezit" : "Gelukkig heb je deze al en hoef je niet helemaal terug",
-      "obstakel_niet" : "Je moet de verf halen, want je hebt het nog niet in je inventory staan"
+      "obstakel_bezit" : "Oh je hebt het al? Mooizo, nu hoef je je niet meer te melden",
+      "obstakel_niet" : "Zie ik nou dat je het nog niet heb gehaald? Ik heb je minstens het drie keer gevraagd.\nGa je maar melden!\nEn volgende keer als je komt heb je verf bij je hè"
     },
     "keuzes": {
       "n": "aula",
@@ -110,7 +104,7 @@ location = {
     }
   },
   "handvaardigheid": {
-    "tekst": "Ben je eigenlijk een beetje creatief. Dan heeft Nobrega wel een opdracht voor jou!",
+    "tekst": "Ben je eigenlijk een beetje creatief. Dan zit je hier bij meneer Nobrega goed. ",
     "keuzes": {
       "n": "kluisjes",
       "o": "nee",
@@ -134,7 +128,7 @@ location = {
   "trappenhuis1b": {
     "tekst": "Je bent nu in het trappenhuis op de eerste verdieping, wil je naar boven of beneden of wil je ergens anders naartoe?",
     "keuzes": {
-      "b": "trappenhuis 1c",
+      "b": "trappenhuis1c",
       "w": "aula balustrade",
       "z": "informatica",
       "o": "Wal",
@@ -144,6 +138,12 @@ location = {
   },
   "aula balustrade": {
     "tekst": "Zo hé, vanaf hier kan je heel de aula zien. Echt te gek!",
+    "obstakel" : { 
+      "obstakel_tekst" : "Zeg jongeman, er is een viering gaande, alleen de TTC mag hierboven komen op de aula balustrade.",
+      "obstakel_object" : "TTC pasje",
+      "obstakel_bezit" : "Oh, je hebt een TTC pasje, nou in dat geval mag je hier gewoon komen. Toch kan ik me niet herinneren dat ik jou eerder gezien heb",
+      "obstakel_niet" : "Zoals ik al had vermoed heb jij geen TTC pasje en mag jij hier dus nu niet zijn. Ga je maar even melden"
+    },
     "keuzes": {
       "n": "Vlaam",
       "w": "biologie",
@@ -154,9 +154,9 @@ location = {
     }
   },
   "Vlaam": {
-    "tekst": "Oh nee, Oh nee, daar is mevrouw Vlaam snel ren weg nu het nog kan. Anders moet je misschien nablijven.",
+    "tekst": "Vlaam: Niet vergeten he, telefoons zijn ten strengste verboden. Als ik er een zie kan je hem om half vijf op komen halen!",
     "obstakel" : {
-      "obstakel_tekst" : "Zeg jongeman, waar denk jij heen te gaan...\nDe lerarenkamer is UITERST verboden voor leerlingen!!",
+      "obstakel_tekst" : "Vlaam: Zeg jongeman, waar denk jij heen te gaan...\nDe lerarenkamer is UITERST verboden voor leerlingen!!",
       "obstakel_object" : "betoog",
       "obstakel_bezit" : "Oh ik zie dat je een betoog  hebt \nDie wil je zeker even in een postvakje van je docent leggen..\nIn dat geval loop maar even door.",
       "obstakel_niet" : "IEDERE LEERLING WEET DAT JE HIER NIET MAG KOMEN \nMELDEN\nNU!!!!"
@@ -171,6 +171,7 @@ location = {
     }
   },
   "lerarenkamer": {
+    "victory" : "JA",
     "keuzes": {
       "z": "Vlaam",
       "o": "nee",
@@ -192,7 +193,11 @@ location = {
     }
   },
   "natuurkunde": {
-    "tekst": "Skip die Kechs ik wil Meijer!! PAS OP MISSCHIEN WORD JE GEËLEKTROCUTEERD, DOOR DEZE NATUURKUNDELERAAR!",
+    "tekst": "Neutronen, elektronen, iets met krachten. Dat kan je hier allemaal leren. ",
+    "object" : {
+      "object_tekst" : "Hey hier ligt een pasje. Er staat TTC op.",
+      "object_soort" : "TTC pasje"
+    },
     "keuzes": {
       "n": "biologie",
       "z": "trappenhuis3b",
@@ -219,13 +224,17 @@ location = {
       "n": "trappenhuis1b",
       "z": "mediatheek",
       "o": "nee",
-      "w": "nee",
+      "w": "Logtenberg",
       "b": "nee",
       "d": "nee"
     }
   },
   "mediatheek": {
     "tekst": "SSSSSSSSSSSSSTTTTT, stilte in de Mediatheek",
+    "object" : {
+      "object_tekst" : "IK WIL DAT HET NU STIL IS!,Oh goedendag, wil je een boek lenen. Ik heb hier nog een woordenboek Grieks voor de liefhebber!",
+      "object_soort" : "woordenboek"
+    },
     "keuzes": {
       "n": "informatica",
       "o": "nee",
@@ -293,6 +302,12 @@ location = {
   },
   "wiskunde": {
     "tekst": "De oppervlakte van een cirkel is πr^2, wiskunde.",
+    "obstakel" : {
+      "obstakel_tekst" : "Welkom bij het beste vak op aarde: Wiskunde. A^2 + b^2 = C^2! Maar dit wist je natuurlijk al lang! Wat veel interessanter is is de binomiale verdeling! Dit kan echt heel gemakkelijk met je GR",
+      "obstakel_object" : "rekenmachine",
+      "obstakel_niet" : "Wil je me nou vertellen dat je je rekenmachine NIET bij je hebt? Oh hij zat wel in je tas? Ja dat zeggen ze allemaal. Ga je maar even melden\nEn volgende keer niet zo bijdehand hè ",
+      "obstakel_bezit" : "En uiteraard heb je er een bij je! Geef maar hier ik laat je wel even zien hoe dat moet!"
+    },
     "keuzes": {
       "w": "trappenhuis1c",
       "o": "nederlands",
@@ -379,12 +394,12 @@ location = {
     }
   },
   "klassieke talen": {
-    "tekst": "Zit jij op het gymnasium? Spreek jij toevallig Griek en Latijns? TEN EERSTE IS HET GRIEKS EN LATIJN EN TEN TWEEDE WE LEREN NIET OM TE PRATEN HET IS EEN DODE TAAL!!!!",
+    "tekst": "Grieks en latijn zijn helemaal geen nutteloze talen, als je er op let zie je dat ze vaak terugkomen in onze huidige leefomgeving. Hier bij Grieks en Latijn leren we je dit soort dingen!",
     "obstakel" : {
-      "obstakel_tekst" : "Je kan je huiswerk niet maken zonder een woordenboek. Ga maar even een woordenboek halen",
+      "obstakel_tekst" : "Ah hallo, welkom bij Latijn en Grieks. Je kan niet vertalen zonder woordenboek, ik neem aan dat je er een bij je hebt...",
       "obstakel_object" : "woordenboek",
-      "obstakel_bezit" : "Je bent echt goed in dit spel, want je hebt het woordenboek. Chapeau",
-      "obstakel_niet" : "Als je het woordenboek had gehad was je nu binnen, maar helaas je moet terug en het woordenboek vinden."
+      "obstakel_bezit" : "Zie je wel, je hebt er gewoon aan gedacht, chapeau",
+      "obstakel_niet" : "Erg zonde dit, had dit niet van jou verwacht. Ga je maar even melden bij de congiërge, je vergeet het bijna elke les"
     },
     "keuzes": {
       "o": "trappenhuis2d",
@@ -417,6 +432,10 @@ location = {
   },
   "tekenen": {
     "tekst": "Dit is tekenen, een lokaal vol verf en mooie kunstwerken van bruggers.",
+    "object" : {
+      "object_tekst" : "Hey, daar ligt een potje verf. Had je die niet ergens voor nodig?" ,
+      "object_soort" : "verf"
+    },
     "keuzes": {
       "n": "frans",
       "o": "nee",
@@ -426,4 +445,16 @@ location = {
       "d": "nee"
     }
   },
+  "Logtenberg" : {
+    "tekst" : "Logtenberg: Hallo, ik ben de informatica docent hiero. Ik zal magister ff hacken en je briefjes kwijtschenden, begin je weer lekker met een schone lei. Fijn toch!",
+    "briefjes" : "JA",
+     "keuzes": {
+      "b": "trappenhuis2b",
+      "w": "kluisjes",
+       "d": "nee",
+       "n": "nee",
+       "o": "nee",
+       "z": "nee"
+      }
+}  
 }
