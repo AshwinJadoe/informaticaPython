be7a56ffbfefea66c697b5f705ae19b6aa43ca70
AshwinJadoe <34601644+AshwinJadoe@users.noreply.github.com>
2019-06-29 13:59:16 +0200

    Final touches, easter egg ook hier toegevoegd

--- textadventure.py
+++ textadventure.py
@@ -4,7 +4,6 @@ from locaties import location
 instructies = "Bewegen door het gebouw doe je met \"n\", \"o\", \"z\", of \"w\" \nAls je bij een trappenhuis aankomt kan je met \"b\" naar boven of met \"d\" naar beneden \nVerder kan je iets(als je iets tegenkomt) in je rugzak doen met \"g\" \nDe inhoud van je rugtas bekijk je met \"i\" \nAls je het spel wilt verlaten toets je \"q\" in \nEn tenslotte, als je de instructies nog een keer wilt horen druk op \"h\" " 
 
 def begin():
-  global naam
   print ("Congiërge: Goedendag, ik ben Yousef de congiërge hier op het St-Maartenscollege.")
   time.sleep(1)
   naam = input("En jij bent?  ")
@@ -22,11 +21,10 @@ def begin():
   print (".\n.\n.\n.")
   time.sleep(1)
   print ("Hey!\n{0} heette je toch?\nNou beste {1} je moet iets voor me doen! \nWij van het leerlingenverzet zijn jaren geleden geschorst van deze school \nAlleen moet ik iets hier iets afleveren\nZou jij dat voor mij willen doen alsjeblief? \nJe hoeft alleen maar deze brief af te leveren in de lerarenkamer, in het postvakje van meneer Logtenberg om precies te zijn \nIk stop m alvast in je tas \nHeel erg bedankt goos! \nSucces!!!".format(naam, naam))
-  
 
-def spel():
-  
-  galg = ["_______\n|     |\n|     O\n|    -|-\n|     |\n|    / \\ \n|\n=======",
+
+def spel(room):
+  animatie = ["_______\n|     |\n|     O\n|    -|-\n|     |\n|    / \\ \n|\n=======",
         "_______\n|     |\n|     O\n|    -|-\n|     |\n|    /\n|\n=======",
         "_______\n|     |\n|     O\n|    -|-\n|     |\n|\n|\n=======",
         "_______\n|     |\n|     O\n|    -|\n|     |\n|\n|\n=======",
@@ -35,7 +33,6 @@ def spel():
         "_______\n|     |\n|\n|\n|\n|\n|\n=======",
         ]
 
-  # Lists containing the words for the game
   vier_letters = ['boom', 'deel', 'ding', 'ring', 'snel', 'ruim', 'duim', 'geel', 'eend']
 
   tien_letters = ['afbakbrood', 'agorafobie', 'autodeuren', 'barricades', 'paaseieren', 'xylofonist']
@@ -47,84 +44,85 @@ def spel():
   ]
 
 
-  #Program Functions
   def pick_diff():
       
-      moeilijkheidsgraad = "Kies een moeilijkheidsgraad!. (Makkelijk, Medium, Moeilijk, Extreem)\n>"
+      keuze_moeilijkheid = "Kies een moeilijkheidsgraad!. (Makkelijk, Medium, Moeilijk, Extreem)\n>"
       choice = ""
       while choice not in ['makkelijk', 'medium', 'moeilijk', 'extreem']:
-          choice = input(moeilijkheidsgraad)
+          choice = input(keuze_moeilijkheid)
           choice = choice.lower()
       change_diff(choice)
 
   def change_diff(level):
       
-      herhaling = "\nJij hebt deze moeilijkheidsgraad gekozen: " + level + ". Wil je het veranderen? [J/N]\n>"
-      antwoord = ""
-      while antwoord not in ['j', 'n']:
-          antwoord = input(herhaling)
-          antwoord = antwoord.lower()
-      if antwoord == 'j':
+      message = "\nJij hebt deze moeilijkheidsgraad gekozen: " + level + ". Wil je het veranderen? [J/N]\n>"
+      answer = ""
+      while answer not in ['j', 'n']:
+          answer = input(message)
+          answer = answer.lower()
+      if answer == 'j':
           pick_diff()
-      if antwoord == 'n':
+      if answer == 'n':
           print("\nSucces!\n")
           choose_word(level)
 
   def choose_word(choice):
       
       if choice == 'makkelijk':
-          word = random.choice(vier_letters)
+          woord = random.choice(vier_letters)
       elif choice == 'medium':
-          word = random.choice(tien_letters)
+          woord = random.choice(tien_letters)
       elif choice == 'moeilijk':
-          word = random.choice(vijfentwintig_letters)
+          woord = random.choice(vijfentwintig_letters)
       elif choice == 'extreem':
-          word = random.choice(vijfendertig_letters)
-      play_game(word)
+          woord = random.choice(vijfendertig_letters)
+      play_game(woord)
 
   def play_game(this_word):
-      word = list(this_word)
-      spaties = "_" * len(word)
-      spaties = list(spaties)
-      geraden = []
-      fout = 6
-      while fout > 0:
-          print("\n" + galg[fout]
-                + "\nJij hebt nog {} kansen over.".format(fout)
-                + "\nJouw woord: " + "".join(spaties)
-                + "\nGeraden letters: " + ", ".join(geraden)
+      woord = list(this_word)
+      blanks = "_" * len(woord)
+      blanks = list(blanks)
+      guessed = []
+      incorrect = 6
+      while incorrect > 0:
+          print("\n" + animatie[incorrect]
+                + "\nJij hebt nog {} kansen over.".format(incorrect)
+                + "\nJouw woord: " + "".join(blanks)
+                + "\nGeraden letters: " + ", ".join(guessed)
               )
           letter = input("Letter: ").lower()
           if len(letter) == 1 and letter.isalpha():
-              if letter in geraden:
+              if letter in guessed:
                   print("\n\nWolla dat heb jij al gezegd!")
                   time.sleep(2)
 
-              elif letter in word:
-                  for index,teken in enumerate(word):
-                      spaties = list(spaties)
-                      if teken == letter:
-                          spaties[index] = letter
-                          current = "".join(spaties)
-                          if spaties == word:
-                              print("\n\nGEFELICITEERD, JIJ HEBT GEWONNEN!!\nJouw woord was " + ''.join(word) + ".\n")
+              elif letter in woord:
+                  for index,character in enumerate(woord):
+                      blanks = list(blanks)
+                      if character == letter:
+                          blanks[index] = letter
+                          current = "".join(blanks)
+                          if blanks == woord:
+                              print("\n\nGEFELICITEERD, JIJ HEBT GEWONNEN!!\nJouw woord was " + ''.join(woord) + ".\n")
                               del location["nederlands"]["spel"]
+                              print ("hier heb je een betoog, zou je deze bij de lerarenkamer willen inleveren")
+                              inventory.append("betoog")
                               engine("nederlands")
 
 
                             
-              elif letter not in word:
-                  fout -= 1
-                  geraden.append(letter)
+              elif letter not in woord:
+                  incorrect -= 1
+                  guessed.append(letter)
           else:
-              print("\n\nPotverjandriedubbeltjes, alleen enkele letters!\n\n")
+              print("\n\n!Potverjandriedubbeltjes, alleen enkele letters!\n\n")
               time.sleep(2)
       else:
-          print(galg[0])
-          print("\nSorry , GAME OVER!\nJouw woord was " + ''.join(word) + ".")
-          briefjehalen()          
+          print(animatie[0])
+          print("\nSorry , GAME OVER!\nJouw woord was " + ''.join(woord) + ".")
+          briefjehalen(levens, room)          
 
-  x= input("Heb je zin om een spelletje te spelen? \nje kan een prijs winnen die je later misschien nog nodig hebt *knipoog*\nMaar let op, als je niet wint, moet je een briefje halen. [ja/nee]")
+  x= input("Heb je zin om een spelletje te spelen? \nje kan een prijs winnen die je later misschien nog nodig hebt *knipoog*\nMaar let op, als je niet wint, moet je een briefje halen. ")
   if x == "ja": 
     print("Oké, laten we beginnen met galgje, als je wint krijg je een object voor je inventory!") 
     pick_diff()
@@ -137,28 +135,28 @@ def spel():
 
 
 
-inventory = ["betoog"]
+inventory = []
 commands_list = ["n", "z", "w", "o", "b", "d", "i", "q", "h"]
 
 current_room = "congiërge"
-begin_kamer = "nederlands"
+begin_kamer = "congiërge"
 levens = 0
 
 
 def roomcheck(room):
   global levens
-  if "obstakel" in location[room]:
-      print (location[room]["obstakel"]["obstakel_tekst"])
-      if location[room]["obstakel"]["obstakel_object"] in inventory:
-        print (location[room]["obstakel"]["obstakel_bezit"])
-        return False
-      else:
-        levens += 1
-        print (location[room]["obstakel"]["obstakel_niet"])
-        briefjehalen (levens)
-          
+  print (location[room]["obstakel"]["obstakel_tekst"])
+  if location[room]["obstakel"]["obstakel_object"] in inventory:
+    print (location[room]["obstakel"]["obstakel_bezit"])
+    del location[room]["obstakel"]
+    engine(room)
   else:
-      return False
+    levens += 1
+    print (location[room]["obstakel"]
+    ["obstakel_niet"])
+    briefjehalen (levens,room)
+
+      
 
 
 def muur(x,room):
@@ -189,7 +187,7 @@ def object_check(room):
 
 def andere_commands (input):
   if input == "i":
-    print(', '.join(inventory))
+    print("Je rugzak bevat hetvolgende: " +', '.join(inventory))
     return True
   elif input == "q":
     quit()
@@ -201,30 +199,36 @@ def andere_commands (input):
     return False
   
 
-def victory(naam):
-  print("Je bent aangekomen bij de befaamde lerarenkamer, een plek waar alleen de elite mag komen! \nElsakkers: Goedendag {0} heette je toch? Beste {1} ik heb goede dingen over jou gehoord\nKan ik iets voor je doen? \nHet vakje van meneer Logtenberg zei je? \nUiteraard, komt voor de bakker!".format(naam, naam))
-  print("game over")
+def victory():
+  print("Je bent aangekomen bij de befaamde lerarenkamer, een plek waar alleen de elite mag komen!")
+  nombre = ("Elsakkers: Goedendag, Ik ben de rector van het St-Maartenscollege. En wie ben jij? ") 
+  print("Oh jij bent {0}, ik heb goede dingen over jou gehoord!\nKan ik iets voor je doen? \nHet vakje van meneer Logtenberg zei je? \nUiteraard, komt voor de bakker!".format(nombre))
+  print("")
+  time.sleep(2)
+  print("*biep biep biep*\nJa hallo met het leerlingenverzet, ik bel je om te vragen of de opdracht voltooid is. \nJa?\nBedankt gozer, je staat bij me in het krijt.\nFijne avond nog, ik hoop je nog eens te spreken")
   quit()
 
-def briefjehalen(levens):
+def briefjehalen(levens, room):
   print("\nYousef: Tjongejonge ben je daar nu al weer?\nHier heb je nog een briefje, dat wordt morgen 8uur melden\nJe hebt nu %s briefje(s), bij 10 word je geschorst! onthoud dat goed!" % levens)  
-  engine(begin_kamer)  
+  engine("congiërge")
 
 
 def engine (room):
-  global naam
+  global levens
   while levens < 10:
     print ("")
     global current_room
-    if roomcheck(room): 
-        engine (begin_kamer)
-    elif spel in  location[room]:
-      victory(naam)
+    if "obstakel" in location[room]: 
+        roomcheck(room)   
+    elif "victory" in location[room]:
+      print ("gelukt")
+      victory()
+    elif "briefjes" in location[room]:
+      print (location[room]["tekst"])
+      levens = 0
+      engine("informatica")
     elif "spel" in location[room]:
-      spel()
-     
-
-      
+      spel(room)
     else:  
       if object_check(room):
         print (location[room]["tekst"])
@@ -239,9 +243,9 @@ def engine (room):
         elif andere_commands(actie) == False:
           for x in (location[room]["keuzes"]):
             if actie == x:  
-              if muur(x, current_room):
-                print("Deze richting kan je niet op")
-                engine(current_room)
+              if muur(x, room):
+                print("Deze richting kan je niet op, je keert terug naar waar je was")
+                engine(room)
             
               else:   
                 current_room = location[room]["keuzes"][actie]
@@ -256,4 +260,8 @@ def engine (room):
   
 
 
+
+  
+
+begin()
 engine(begin_kamer)
